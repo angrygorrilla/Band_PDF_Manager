d0693d653a3fae180a3d052bc66fba5a45e9dacc
angrygorrilla <gchapm02@mail.uoguelph.ca>
2025-04-09 17:44:51 -0400

    incomplete folder download added

--- band_pdf_manager/src/endpoints/drive_endpoint.py
+++ band_pdf_manager/src/endpoints/drive_endpoint.py
@@ -5,21 +5,93 @@ from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
+import google.auth
+from googleapiclient.http import MediaIoBaseDownload
+import io
+
+def download_file(real_file_id):
+  """Downloads a file
+  Args:
+      real_file_id: ID of the file to download
+  Returns : IO object with location.
+
+  Load pre-authorized user credentials from the environment.
+  TODO(developer) - See https://developers.google.com/identity
+  for guides on implementing OAuth2 for the application.
+  """
+  creds=get_creds()
+
+  try:
+    # create drive api client
+    service = build("drive", "v3", credentials=creds)
+
+    file_id = real_file_id
+
+    # pylint: disable=maybe-no-member
+    request = service.files().get_media(fileId=file_id)
+    file = io.BytesIO()
+    downloader = MediaIoBaseDownload(file, request)
+    done = False
+    while done is False:
+      status, done = downloader.next_chunk()
+      print(f"Download {int(status.progress() * 100)}.")
+
+  except HttpError as error:
+    print(f"An error occurred: {error}")
+    file = None
+
+  return file.getvalue()
 
 # If modifying these scopes, delete the file token.json.
 SCOPES = ["https://www.googleapis.com/auth/drive.metadata.readonly"]
 
-
-def main():
+#get the top n files
+def top_file_names(number_of_files):
   """Shows basic usage of the Drive v3 API.
   Prints the names and ids of the first 10 files the user has access to.
   """
-  creds = None
   # The file token.json stores the user's access and refresh tokens, and is
   # created automatically when the authorization flow completes for the first
   # time.
+  creds=get_creds()
+
+  try:
+    service = build("drive", "v3", credentials=creds)
+    
+    # Call the Drive v3 API
+    results = (
+        service.files()
+        .list(pageSize=number_of_files, fields="nextPageToken, files(id, name)")
+        .execute()
+    )
+
+    #query terms
+        #mimeType = 'application/vnd.google-apps.folder'
+        #Files within a collection (for example, the folder ID in the parents collection)
+
+    items = results.get("files", [])
+
+    if not items:
+      print("No files found.")
+      return
+    print("Files:")
+    #items from this api include a name and an id
+    for count,item in enumerate(items):
+        print(f"{item['name']} ({item['id']})")
+  except HttpError as error:
+    # TODO(developer) - Handle errors from drive API.
+    print(f"An error occurred: {error}")
+
+
+
+#use a google token saved in json format to certify using google drive apis
+def get_creds():
+    # The file token.json stores the user's access and refresh tokens, and is
+  # created automatically when the authorization flow completes for the first
+  # time.
+  creds=None
   if os.path.exists("token.json"):
-    creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+    creds = Credentials.from_authorized_user_file("secrets/token.json", SCOPES)
   # If there are no (valid) credentials available, let the user log in.
   if not creds or not creds.valid:
     if creds and creds.expired and creds.refresh_token:
@@ -30,36 +102,21 @@ def main():
       )
       creds = flow.run_local_server(port=0)
     # Save the credentials for the next run
-    with open("token.json", "w") as token:
-      token.write(creds.to_json())
-
-  try:
-    service = build("drive", "v3", credentials=creds)
-
-    # Call the Drive v3 API
-    results = (
-        service.files()
-        .list(pageSize=10, fields="nextPageToken, files(id, name)")
-        .execute()
-    )
-
-    #query terms
-        #mimeType = 'application/vnd.google-apps.folder'
-        #Files within a collection (for example, the folder ID in the parents collection)
-    items = results.get("files", [])
-
-    if not items:
-      print("No files found.")
-      return
-    print("Files:")
-    for count,item in enumerate(items):
-        if count==0:
-            print(item)
-        #print(f"{item['name']} ({item['id']})")
-  except HttpError as error:
-    # TODO(developer) - Handle errors from drive API.
-    print(f"An error occurred: {error}")
-
+    #with open("secrets/token.json", "w") as token:
+    #  token.write(creds.to_json())
+    return creds
 
 if __name__ == "__main__":
-  main()
+  #top_file_names(20)
+  
+
+ 
+
+  #download file does not work on folder - need to do a parent search using it
+  #example of folder to take files from:
+  #GCB FALL - TUBA (1X7tY37gwMj4otUOYMo98wO31NOgqucOZ)
+
+
+#id's from the other service don't seem to work - need to look into it.
+  #example of a file: when robins appear (1naNF8R2M0BdpiZF1Q60Ax_yA1Re7LKdk)
+  download_file('1naNF8R2M0BdpiZF1Q60Ax_yA1Re7LKdk')
