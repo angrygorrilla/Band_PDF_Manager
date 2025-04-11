246f7249fc0c6d575b939805640c027ed2550721
angrygorrilla <gchapm02@mail.uoguelph.ca>
2025-04-10 22:47:12 -0400

    happy path completed for pdf splitting

--- band_pdf_manager/src/endpoints/pdf_page_titles.py
+++ band_pdf_manager/src/endpoints/pdf_page_titles.py
@@ -63,7 +63,7 @@ def pdf_to_instruments(pdf_file,max_pages=-1):
             instruments.append(prev_instrument)
     
     
-    return page_instrument_references
+    return instruments
 
 
 #return the instrument from each page of text
@@ -112,12 +112,12 @@ def flatten(input_list):
 #turn a list of pages with elements into a list of page slices
 #instrument list is a 1-d array of 1 instrument on each page
 def instrument_pages_to_slices(instrument_list):
-    instrument_set= set(flatten(instrument_list))
+    instrument_set= set(instrument_list)
     slices=[]
     instruments=[]
     for instrument in instrument_set:
         indices = [index for index, element in enumerate(instrument_list) if element == instrument]
-        print(indices)  # will print [1, 3]
+        print(indices) 
         first=min(indices)
         last=max(indices)
         instruments.append(instrument)
@@ -151,10 +151,8 @@ def test():
     text = [['Written in 2019 for the University of Wisconsin Stevens Point bands, Michael S Butler; director:', 'Dedicated to Donald E. Greene and in celebration of the 12Sth anniversary of UWSP', 'Trumpet in Bb 1', 'RIVER OF STARS', 'Barbara York', 'Allegro', '2', '72', '(to open)', '(solo)', '10', 'mute', 'm', 'Allegretto marcato', '112', 'Meno mosso', '24', '2', 'poco rit.', '3', '29', 'open', 'mp', '43', '55', '65', '73', 'Copyright 0 2019 Cimarron Music Press. All Rights Reserved.', 'www.CimarronMusiccom'],
      ['81', 'm', 'molto rit', '93', '=', '60', '167', '175', '101', '183', '76', '109', 'my]', 'rit.', '119| Allegro', '120', '(to mute)', '2', '/191', '60', 'm', '130', 'poco rit.', '96', '(solo)', 'mute', 'mp', 'poco meno mosso', '= 80', '98', '199|', '2091', '35', '(to open)', '138]', '= 76', 'rit', '3', '5', '218/', '148|', '=', '112', 'open', '158]', '(to mute)', 'mute', '6', 'm', 'mp', 'Trumpet in Bb 1 - 2', 'Trumpet in Bb 1 - 3'],
 ['Written in 2019 for the University of Wisconsin Stevens Point bands, Michael S Butler; director:', 'Dedicated to Donald E. Greene and in celebration of the 12Sth anniversary of UWSP', 'Trumpet in Bb 1', 'RIVER OF STARS', 'Barbara York', 'Allegro', '2', '72', '(to open)', '(solo)', '10', 'mute', 'm', 'Allegretto marcato', '112', 'Meno mosso', '24', '2', 'poco rit.', '3', '29', 'open', 'mp', '43', '55', '65', '73', 'Copyright 0 2019 Cimarron Music Press. All Rights Reserved.', 'www.CimarronMusiccom']]
-    pdf_file='Hands of Mercy - Low Brass.pdf'
-    instrument_list=(pdf_to_instruments(pdf_file,max_pages=3))
-    print('instrument list')
-    print(instrument_list)
+    pdf_file='Hands of Mercy - Low Brass.pdf'    
+    instrument_list=(pdf_to_instruments(pdf_file))
     instruments,slices=instrument_pages_to_slices(instrument_list)
     print(instruments)
     print('slices')
